22805ae2ef9fa4141037389825f6a5a8a4cd3887
Andres Escobedo <43122995+aescobed@users.noreply.github.com>
2020-06-09 16:35:37 -0700

    OOP refresher with python

--- Udemy/Section 5/Neural Net Intro/Neural Net Intro/Neural_Net_Intro.py
+++ Udemy/Section 5/Neural Net Intro/Neural Net Intro/Neural_Net_Intro.py
@@ -1,19 +1,65 @@
 
-import tensorflow as tf
+"""
+z = wx + b where w are the weights, x are the inputs and b is the bias term.
+    z is the sum of all the inputs multiplied by the weights plus the bias term.
 
-# Using the estimator API 
+sigmoid function
+    f(z) = 1/[1 + e^(-z)]
+
+ReLU    rectified linear unit
+    max(0,z)
+        makes answer positive
+
+Quadratic cost function
+    C = sumOf(y-a)^2 / n
+        larger errors are  more prominent due to the squaring
+
+cross entropy
+    c = (-1/n) * sumOf(y ln(a) + 1-y) ln(1-a)
+
+backpropagation
+    A way to actually learn and fix the error which is predicted
+    Calculates error at the output and then distributes back through the network layers
+    Requires a known desired output for each input value
 
 
-
-# Using only one feature in the feature column
-
-feature_cols = [tf.feature_column.numeric_column('x', shape=[1])]
-
-estimator = tf.estimator.LinearRegressor(feature_columns=feature_cols)
+gradient descent
+    1st order minimizer
 
 
+"""
+
+# executes upon creation
+class SimpleClass():
+
+        def __init__(self, name): 
+            print("hello")
+
+        def yell(self):
+            print("YELLING")
 
 
-x_train, x_eval, y_train, y_eval = train_test_split(x_data, t_true, test_size=0.3, random_state=101)
+s = "world"
 
-print(x_train.shape)
+type(s)
+
+# will print hello
+x = SimpleClass()
+
+#shows you what it is and where in the memory it is stored
+print(x)
+
+
+x.yell()
+
+class ExtendedClass(SimpleClass):
+
+    def __init__(self):
+
+        # run the init method in the simple class
+        super().__init__()
+        print("EXTEND!")
+
+y = ExtendedClass()
+y.yell()
+
