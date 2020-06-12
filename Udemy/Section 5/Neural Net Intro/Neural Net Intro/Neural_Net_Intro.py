5a94305b345771dd46d08954fb0ff43e38601926
Andres Escobedo <43122995+aescobed@users.noreply.github.com>
2020-06-11 19:58:51 -0700

    Neural net intro
    sklearn module still not working. I think I have the wrong version

--- Udemy/Section 5/Neural Net Intro/Neural Net Intro/Neural_Net_Intro.py
+++ Udemy/Section 5/Neural Net Intro/Neural Net Intro/Neural_Net_Intro.py
@@ -189,7 +189,7 @@ sess = Session()
 
 result = sess.run(operation=z, feed_dict={x:10})
 
-print("result: ",  result)
+#print("result: ",  result)
 
 
 # new matrix multiplication example
@@ -209,6 +209,36 @@ sess = Session()
 
 result = sess.run(operation=z, feed_dict={x:10})
 
-print("result2: ",  result)
+#print("result2: ",  result)
+
+
+
+# CLASSIFICATION
+
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+sample_z = np.linspace(-10, 10, 100)
+
+sample_a = sigmoid(sample_z)
+
+#plt.plot(sample_z, sample_a)
+#plt.show()
+
+
+class Sigmoid(Operation):
+
+    def __init__(self,z):
+
+        super().__init__([z])
+
+    def compute(self, z_val):
+        return 1 / (1 + np.exp(-z))
+
+import sklearn as sk
+
+print(sk.__version__)
 
 
